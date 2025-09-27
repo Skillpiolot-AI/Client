6cdba09536a1221d26514bf8b6fcc35d7be79f73
ujjwaljha1 <ujjwaljha744@gmail.com>
2025-09-27 16:15:34 +0530

    Navbar update

---
+++ dist/assets/index-CG9iQfnH.js
@@ -0,0 +1,1 @@
+import{g as a,a as e,c as t}from"./index-BmB2n9Mv.js";const r={renderer:t,...e,...a};var n=r;export{n as default};
--- src/config.jsx
+++ src/config.jsx
@@ -3,10 +3,11 @@
 const config = {
   // Base URL for API requests
   // API_BASE_URL: 'https://sih-server-production.up.railway.app/api',
-  API_BASE_URL: '  http://localhost:3001/api',
-  API_BASE_URL1: '  http://localhost:3001',
-  //API_BASE_URL: 'https://mentorship-server-production.up.railway.app/api',
-  //   API_BASE_URL1: 'https://mentorship-server-production.up.railway.app/'
+//  API_BASE_URL: '  http://localhost:3001/api',
+  //API_BASE_URL1: '  http://localhost:3001',
+  API_BASE_URL: 'https://server-production-fec1.up.railway.app/api',
+   API_BASE_URL1: 'https://server-production-fec1.up.railway.app'
+  
   //Hello world 
   // For example:
   //blaa blaa
