c7d24803f8bb24381b04e92e86050885b1b97e00
Ujjwal <ujjwaljha744@gmail.com>
2025-10-15 09:48:29 +0530

    Api changes

--- src/config.jsx
+++ src/config.jsx
@@ -5,8 +5,8 @@ const config = {
   // API_BASE_URL: 'https://sih-server-production.up.railway.app/api',
 //  API_BASE_URL: '  http://localhost:3001/api',
   //API_BASE_URL1: '  http://localhost:3001',
-  API_BASE_URL: 'https://server-production-fec1.up.railway.app/api',
-   API_BASE_URL1: 'https://server-production-fec1.up.railway.app'
+  API_BASE_URL: 'https://server-production-1b3d.up.railway.app/api',
+   API_BASE_URL1: 'https://server-production-1b3d.up.railway.app'
   
   //Hello world 
   // For example:
