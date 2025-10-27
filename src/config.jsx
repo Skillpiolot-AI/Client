fe23c88b76004d4013a1d6f8313fb3bd8c028c6a
ujjwaljha1 <ujjwaljha744@gmail.com>
2025-10-27 11:41:11 +0530

    Accepted all incoming changes from backup-feature-update into landing-page

--- src/config.jsx
+++ src/config.jsx
@@ -3,10 +3,10 @@
 const config = {
   // Base URL for API requests
   // API_BASE_URL: 'https://sih-server-production.up.railway.app/api',
- //API_BASE_URL: '  http://localhost:3001/api',
- // API_BASE_URL1: '  http://localhost:3001',
-    API_BASE_URL: 'https://server-production-1b3d.up.railway.app/api',
-   API_BASE_URL1: 'https://server-production-1b3d.up.railway.app'
+//API_BASE_URL: '  http://localhost:3001/api',
+ //API_BASE_URL1: '  http://localhost:3001',
+    API_BASE_URL: 'https://server-x82u.onrender.com/api',
+   API_BASE_URL1: 'https://server-x82u.onrender.com'
 };
 
 export default config;
