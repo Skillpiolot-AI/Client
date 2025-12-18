cb4d1012fd669015595237a97ff4e0e3ce0b76ac
ujjwaljha1 <ujjwaljha744@gmail.com>
2025-12-19 00:24:29 +0530

    feat: externalize API URLs into `.env` files and update config to use environment variables.

---
+++ dist/assets/index-DOlrUUf0.js
@@ -0,0 +1,1 @@
+import{T as a,V as e,W as t}from"./index-BnxrnJgh.js";const r={renderer:t,...e,...a};var n=r;export{n as default};
--- src/config.jsx
+++ src/config.jsx
@@ -1,13 +1,9 @@
 // src/config.js
 
 const config = {
-  // Base URL for API requests
-  // API_BASE_URL: 'https://sih-server-production.up.railway.app/api',
-  API_BASE_URL: 'http://localhost:3001/api',
-  API_BASE_URL1: 'http://localhost:3001',
- // API_BASE_URL: 'https://server-x82u.onrender.com/api',
- // API_BASE_URL1: 'https://server-x82u.onrender.com'
+  // Base URL for API requests - uses environment variables with fallbacks
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
+  API_BASE_URL1: import.meta.env.VITE_API_BASE_URL1 
 };
 
 export default config;
-
